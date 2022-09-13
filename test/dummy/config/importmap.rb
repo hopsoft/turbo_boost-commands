917d66c0378f3740a67c8d95607b147477e75703
Nate Hopkins <natehop@gmail.com>
2022-09-13 15:09:19 -0600

    Fix js lib pathing and readme example (#5)

--- test/dummy/config/importmap.rb
+++ test/dummy/config/importmap.rb
@@ -10,6 +10,6 @@ pin "turbo_ready", to: "https://ga.jspm.io/npm:turbo_ready@0.0.7/app/assets/buil
 # this pin works because of the link_tree directive in: test/dummy/app/assets/config/manifest.js
 # that points to the relative path of the build directory for turbo_reflex.js
 # SEE: package.json for details on the build script
-pin "turbo_reflex", to: "turbo_reflex.min.js"
+pin "turbo_reflex", to: "turbo_reflex.js"
 
 pin "application", preload: true
