1a3415a05d30c447d2cf8f3a13d7b387f501faad
Nate Hopkins (hopsoft) <natehop@gmail.com>
2022-09-12 19:10:18 -0600

    Cleanup

--- app/javascript/frame_sources.js
+++ app/javascript/frame_sources.js
@@ -19,24 +19,10 @@ addEventListener('turbo:before-fetch-response', event => {
   })
 })
 
-// fires when a frame element is navigated
+// fires when a frame element is navigated and finishes loading
 addEventListener('turbo:frame-load', event => {
   const frame = event.target
   frame.dataset.turboReflexSrc =
     frameSources[frame.id] || frame.src || frame.dataset.turboReflexSrc
   delete frameSources[frame.id]
-})
-
-// fires before a stream response is rendered
-addEventListener('turbo:before-stream-render', event => {
-  const stream = event.target
-  const frame = stream.templateContent.querySelector('turbo-frame')
-  if (!frame) return
-  const frameId = frame.id
-  setTimeout(() => {
-    const f = document.getElementById(frameId)
-    f.dataset.turboReflexSrc =
-      frameSources[frame.id] || f.src || f.dataset.turboReflexSrc
-    delete frameSources[frameId]
-  }, 100)
 })
