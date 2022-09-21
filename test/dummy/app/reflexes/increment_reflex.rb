70d28fc931d6d82e91d0e3042eae89a593658756
Nate Hopkins (hopsoft) <natehop@gmail.com>
2022-09-21 11:12:38 -0600

    Iterating on new demos

--- test/dummy/app/reflexes/increment_reflex.rb
+++ test/dummy/app/reflexes/increment_reflex.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
+# Reflex instances have access to the following methods and properties.
+#
+# * controller - the Rails controller processing the HTTP request
+# * element - a struct that represents the DOM element that triggered the reflex
+# * turbo_stream - a Turbo Stream TagBuilder
+# * turbo_streams - a list of Turbo Streams to append to the response
+#
 class IncrementReflex < TurboReflex::Base
   def increment
     controller.instance_variable_set :@count, element.data_count.to_i + 1
-  end
-
-  def decrement
-    controller.instance_variable_set :@count, element.data_count.to_i - 1
   end
 end
