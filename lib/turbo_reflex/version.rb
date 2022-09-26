c47fa5dd3bcd44a6eb49d4a32f8d8d124e85199b
Nate Hopkins (hopsoft) <natehop@gmail.com>
2022-09-26 11:48:16 -0600

    Version bump

--- lib/turbo_reflex/version.rb
+++ lib/turbo_reflex/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module TurboReflex
-  VERSION = "0.0.3"
+  VERSION = "0.0.4"
 end
--- test/dummy/config/importmap.rb
+++ test/dummy/config/importmap.rb
@@ -3,8 +3,8 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.1.0/dist/stimulus.js"
-pin "@hotwired/turbo", to: "https://ga.jspm.io/npm:@hotwired/turbo@7.2.0-rc.3/dist/turbo.es2017-esm.js"
-pin "@hotwired/turbo-rails", to: "https://ga.jspm.io/npm:@hotwired/turbo-rails@7.2.0-rc.3/app/javascript/turbo/index.js"
+pin "@hotwired/turbo", to: "https://ga.jspm.io/npm:@hotwired/turbo@7.2.0/dist/turbo.es2017-esm.js"
+pin "@hotwired/turbo-rails", to: "https://ga.jspm.io/npm:@hotwired/turbo-rails@7.2.0/app/javascript/turbo/index.js"
 pin "@rails/actioncable/src", to: "https://ga.jspm.io/npm:@rails/actioncable@7.0.4/src/index.js"
 pin "flowbite", to: "https://ga.jspm.io/npm:flowbite@1.5.3/dist/flowbite.js"
 pin "turbo_ready", to: "https://ga.jspm.io/npm:turbo_ready@0.1.0/app/javascript/turbo_ready.js"
