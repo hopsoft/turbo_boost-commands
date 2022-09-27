61fd5ef693882d7575fb33730d393671324f4d2e
Nate Hopkins (hopsoft) <natehop@gmail.com>
2022-09-26 22:36:17 -0600

    Update code examples for multi frame

--- test/dummy/app/views/codes/increment/multiple_frame/reflex.rb
+++ test/dummy/app/views/codes/increment/multiple_frame/reflex.rb
@@ -10,10 +10,8 @@
 class IncrementReflex < TurboReflex::Base
   delegate :session, to: :controller
 
-  # triggered client-side by elements with `data-turbo-reflex="IncrementReflex#increment"`
-  # performed server-side by an implicit controller before_action
   def increment
-    # update the state held in sesion
-    session[:count] = session.fetch(:count, 0) + 1
+    key = "frame-#{element.data_id}-count"
+    session[key] = session.fetch(key, 0) + 1
   end
 end
--- test/dummy/app/views/codes/increment/rpc_over_http/reflex.rb
+++ test/dummy/app/views/codes/increment/rpc_over_http/reflex.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+# Reflex instances have access to the following methods and properties.
+#
+# * controller ..... The Rails controller processing the HTTP request
+# * element ........ A struct that represents the DOM element that triggered the reflex
+# * turbo_stream ... A Turbo Stream TagBuilder
+# * turbo_streams .. A list of Turbo Streams to append to the response
+#
 class IncrementReflex < TurboReflex::Base
   delegate :session, to: :controller
 
