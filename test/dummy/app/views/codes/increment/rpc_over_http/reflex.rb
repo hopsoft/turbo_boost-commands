c6d6ea5baecfa376b332ff037a84f89b0982da5e
Nate Hopkins (hopsoft) <natehop@gmail.com>
2022-09-26 23:04:30 -0600

    Update code samples for RPC over HTTP

--- test/dummy/app/views/codes/increment/rpc_over_http/reflex.rb
+++ test/dummy/app/views/codes/increment/rpc_over_http/reflex.rb
@@ -12,7 +12,7 @@ class IncrementReflex < TurboReflex::Base
   delegate :session, to: :controller
 
   def increment
-    session[:rpc_count] = session.fetch(:rpc_count, 0) + 1
-    turbo_streams << turbo_stream.replace("rpc", partial: "path/to/partial")
+    session[:count] = session.fetch(:count, 0) + 1
+    turbo_streams << turbo_stream.replace("rpc", partial: "demos/increment")
   end
 end
