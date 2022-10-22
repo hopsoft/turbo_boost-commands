f677b734ceea73d0f0b7d1713b62d68929d6e1d0
Nate Hopkins (hopsoft) <natehop@gmail.com>
2022-10-22 08:20:06 -0400

    Update demos to continue to use session for "data"

--- test/dummy/app/reflexes/counter_reflex.rb
+++ test/dummy/app/reflexes/counter_reflex.rb
@@ -4,7 +4,7 @@ class CounterReflex < TurboReflex::Base
   delegate :session, to: :controller
 
   def increment
-    key = element.dataset.ui_state_key
-    ui_state[key] = ui_state.fetch(key, 0) + 1
+    key = element.dataset.session_key
+    session[key] = session[key].to_i + 1
   end
 end
