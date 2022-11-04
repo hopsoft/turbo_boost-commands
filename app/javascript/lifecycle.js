17c3acc4ad35eea86105bd6bc22612f5c7862e32
Nate Hopkins (hopsoft) <natehop@gmail.com>
2022-11-04 16:57:13 -0600

    Remove unused import

--- app/javascript/lifecycle.js
+++ app/javascript/lifecycle.js
@@ -1,6 +1,5 @@
 import activity from './activity'
 import { dispatch, lifecycleEvents as events } from './events'
-import meta from './meta'
 
 function finish (event) {
   event.detail.endedAt = new Date().getTime()
