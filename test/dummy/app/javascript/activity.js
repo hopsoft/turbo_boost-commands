be8c2b90d37b164a4465b621256b0640396e73b3
Nate Hopkins (hopsoft) <natehop@gmail.com>
2022-11-07 21:32:21 -0500

    Update activity indication example

--- test/dummy/app/javascript/activity.js
+++ test/dummy/app/javascript/activity.js
@@ -1,8 +1,10 @@
 addEventListener(TurboReflex.events.start, event => {
-  event.target.classList.add('animate-pulse')
+  event.target.classList.add('animate-bounce')
   document.body.classList.add('busy')
 })
 
 addEventListener(TurboReflex.events.finish, event => {
+  if (event.target.nodeType === Node.ELEMENT_NODE)
+    event.target.classList.remove('animate-bounce')
   document.body.classList.remove('busy')
 })
