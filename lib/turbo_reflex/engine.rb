3f05594f8082d82868ce78b16ab3bf001f1373c3
Nate Hopkins (hopsoft) <natehop@gmail.com>
2022-11-23 12:18:16 -0700

    Sort config options

--- lib/turbo_reflex/engine.rb
+++ lib/turbo_reflex/engine.rb
@@ -12,8 +12,8 @@ module TurboReflex
 
   class Engine < ::Rails::Engine
     config.turbo_reflex = ActiveSupport::OrderedOptions.new
-    config.turbo_reflex[:validate_client_token] = true
     config.turbo_reflex[:max_cookie_size] = ActionDispatch::Cookies::MAX_COOKIE_SIZE / 2
+    config.turbo_reflex[:validate_client_token] = true
 
     initializer "turbo_reflex.configuration" do
       Mime::Type.register "text/vnd.turbo-reflex.html", :turbo_reflex
