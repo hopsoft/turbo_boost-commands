63a16b1b8c42717a109e2c09ecdb4d721abd999c
Nate Hopkins (hopsoft) <natehop@gmail.com>
2022-11-24 07:17:13 -0700

    Update demo docs/comments for config options

--- test/dummy/config/initializers/turbo_reflex.rb
+++ test/dummy/config/initializers/turbo_reflex.rb
@@ -7,8 +7,10 @@
 # - Rails.application.config.turbo_reflex
 #
 # Options:
-# - max_cookie_size - Max size for the state management cookie in bytes (ActionDispatch::Cookies::MAX_COOKIE_SIZE / 2)
-# - validate_client_token - Whether or not to validate the client token when invoking reflexes (true)
+# - max_cookie_size, Max size for the state management cookie in bytes (ActionDispatch::Cookies::MAX_COOKIE_SIZE / 3)
+# - validate_client_token, opt-(in/out) of client token validation (true)
+# - apply_client_state_overrides, opt-(in/out) of client state overrides (false)
+# - apply_server_state_overrides, opt-(in/out) of server state overrides (false)
 #
 TurboReflex.config.tap do |config|
   # NOTE: We're being extra cautious with cookie size by targeting a max of 4 kilobytes for ALL cookies across the domain.
@@ -16,4 +18,6 @@ TurboReflex.config.tap do |config|
   # SEE: https://stackoverflow.com/questions/52203972/maximum-cookie-size-of-current-browsers-year-2018
   config.max_cookie_size = 1.kilobyte
   config.validate_client_token = true
+  config.apply_client_state_overrides = false
+  config.apply_server_state_overrides = false
 end
