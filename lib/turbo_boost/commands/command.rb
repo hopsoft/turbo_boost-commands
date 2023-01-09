c15808b6e38971104704023cf3089dc99485b371
Nate Hopkins <natehop@gmail.com>
2023-01-09 10:58:55 -0700

    Add some instance variable helper methods (#68)

--- lib/turbo_boost/commands/command.rb
+++ lib/turbo_boost/commands/command.rb
@@ -9,18 +9,20 @@ require_relative "command_callbacks"
 # Commands are executed via a before_action in the Rails controller lifecycle.
 # They have access to the following methods and properties.
 #
-# * controller .................. The Rails controller processing the HTTP request
-# * css_id_selector ............. Returns a CSS selector for an element `id` i.e. prefixes with `#`
-# * dom_id ...................... The Rails dom_id helper
-# * dom_id_selector ............. Returns a CSS selector for a dom_id
-# * element ..................... A struct that represents the DOM element that triggered the command
-# * morph ....................... Appends a Turbo Stream to morph a DOM element
-# * params ...................... Commands specific params (frame_id, element, etc.)
-# * render ...................... Renders Rails templates, partials, etc. (doesn't halt controller request handling)
-# * renderer .................... An ActionController::Renderer
-# * state ....................... An object that stores ephemeral `state`
-# * turbo_stream ................ A Turbo Stream TagBuilder
-# * turbo_streams ............... A list of Turbo Streams to append to the response (also aliased as streams)
+# * controller ...................... The Rails controller processing the HTTP request
+# * convert_to_instance_variables ... Converts a Hash to instance variables
+# * css_id_selector ................. Returns a CSS selector for an element `id` i.e. prefixes with `#`
+# * dom_id .......................... The Rails dom_id helper
+# * dom_id_selector ................. Returns a CSS selector for a dom_id
+# * element ......................... A struct that represents the DOM element that triggered the command
+# * morph ........................... Appends a Turbo Stream to morph a DOM element
+# * params .......................... Commands specific params (frame_id, element, etc.)
+# * render .......................... Renders Rails templates, partials, etc. (doesn't halt controller request handling)
+# * renderer ........................ An ActionController::Renderer
+# * state ........................... An object that stores ephemeral `state`
+# * transfer_instance_variables ..... Transfers all instance variables to another object
+# * turbo_stream .................... A Turbo Stream TagBuilder
+# * turbo_streams ................... A list of Turbo Streams to append to the response (also aliased as streams)
 #
 # They also have access to the following class methods:
 #
@@ -90,9 +92,22 @@ class TurboBoost::Commands::Command
     @turbo_streams = Set.new
   end
 
-  # Abdstract `perform` method, overridde in subclassed commands
+  # Abstract `perform` method, overridde in subclassed commands
   def perform
     raise NotImplementedError, "#{self.class.name} must implement the `perform` method!"
+  end
+
+  # Converts a Hash to instance variables
+  def convert_to_instance_variables(hash = {})
+    return unless hash.present?
+    hash.each { |key, value| instance_variable_set :"@#{key}", value }
+  end
+
+  # Transfers all instance variables to another object
+  def transfer_instance_variables(object)
+    instance_variables.each do |name|
+      object.instance_variable_set name, instance_variable_get(name)
+    end
   end
 
   def dom_id_selector(...)
--- test/dummy/app/views/codes/increment/multiple_frame/command.rb
+++ test/dummy/app/views/codes/increment/multiple_frame/command.rb
@@ -6,19 +6,20 @@
 # Commands are executed via a before_action in the Rails controller lifecycle.
 # They have access to the following methods and properties.
 #
-# * controller .................. The Rails controller processing the HTTP request
-# * css_id_selector ............. Returns a CSS selector for an element `id` i.e. prefixes with `#`
-# * dom_id ...................... The Rails dom_id helper
-# * dom_id_selector ............. Returns a CSS selector for a dom_id
-# * element ..................... A struct that represents the DOM element that triggered the command
-# * morph ....................... Appends a Turbo Stream to morph a DOM element
-# * params ...................... Commands specific params (frame_id, element, etc.)
-# * render ...................... Renders Rails templates, partials, etc. (doesn't halt controller request handling)
-# * render_response ............. Renders a full controller response
-# * renderer .................... An ActionController::Renderer
-# * state ....................... An object that stores ephemeral `state`
-# * turbo_stream ................ A Turbo Stream TagBuilder
-# * turbo_streams ............... A list of Turbo Streams to append to the response (also aliased as streams)
+# * controller ...................... The Rails controller processing the HTTP request
+# * convert_to_instance_variables ... Converts a Hash to instance variables
+# * css_id_selector ................. Returns a CSS selector for an element `id` i.e. prefixes with `#`
+# * dom_id .......................... The Rails dom_id helper
+# * dom_id_selector ................. Returns a CSS selector for a dom_id
+# * element ......................... A struct that represents the DOM element that triggered the command
+# * morph ........................... Appends a Turbo Stream to morph a DOM element
+# * params .......................... Commands specific params (frame_id, element, etc.)
+# * render .......................... Renders Rails templates, partials, etc. (doesn't halt controller request handling)
+# * renderer ........................ An ActionController::Renderer
+# * state ........................... An object that stores ephemeral `state`
+# * transfer_instance_variables ..... Transfers all instance variables to another object
+# * turbo_stream .................... A Turbo Stream TagBuilder
+# * turbo_streams ................... A list of Turbo Streams to append to the response (also aliased as streams)
 #
 # They also have access to the following class methods:
 #
