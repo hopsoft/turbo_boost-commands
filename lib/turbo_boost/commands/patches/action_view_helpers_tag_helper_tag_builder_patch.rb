a88b62f69f0639d021a785beca3f63ca5615f3cf
mansakondo <mansakondo22@gmail.com>
2023-04-15 21:20:10 +0200

    Fix Ruby syntax error

--- lib/turbo_boost/commands/patches/action_view_helpers_tag_helper_tag_builder_patch.rb
+++ lib/turbo_boost/commands/patches/action_view_helpers_tag_helper_tag_builder_patch.rb
@@ -3,8 +3,8 @@
 require_relative "../attribute_hydration"
 
 module TurboBoost::Commands::Patches::ActionViewHelpersTagHelperTagBuilderPatch
-  def tag_options(options, ...)
+  def tag_options(options, *args, **kwargs, &block)
     dehydrated_options = TurboBoost::Commands::AttributeHydration.dehydrate(options)
-    super(dehydrated_options, ...)
+    super(dehydrated_options, *args, **kwargs, &block)
   end
 end
