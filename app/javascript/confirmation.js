9ea0d8d33bca041e967977025404a9fe8b42c059
Nate Hopkins (hopsoft) <natehop@gmail.com>
2023-07-19 16:54:11 -0600

    Delegate confirm to Turbo when appropriate

--- app/javascript/confirmation.js
+++ app/javascript/confirmation.js
@@ -5,12 +5,27 @@ const confirmation = {
   method: message => Promise.resolve(confirm(message))
 }
 
+const isTurboMethod = event => event.detail.driver === 'method'
+
+const isTurboForm = event => {
+  if (event.detail.driver !== 'form') return false
+
+  const element = event.target
+  const frame = element.closest('turbo-frame')
+  const target = element.closest(`[${schema.frameAttribute}]`)
+  return !!(frame || target)
+}
+
+const delegate = event => isTurboMethod(event) || isTurboForm(event)
+
 document.addEventListener(commandEvents.start, async event => {
   const message = event.target.getAttribute(schema.confirmAttribute)
-  if (message) {
-    const confirmed = await confirmation.method(message)
-    if (!confirmed) event.preventDefault()
-  }
+  if (!message) return
+
+  event.detail.confirmation = true
+
+  if (delegate(event)) return // delegate confirmation handling to Turbo
+  if (await confirmation.method(message)) event.preventDefault()
 })
 
 export default confirmation
