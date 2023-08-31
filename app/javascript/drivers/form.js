53c7d5fc50a95900803d19060739bb01bffe8395
Nate Hopkins (hopsoft) <natehop@gmail.com>
2023-08-31 17:43:43 -0600

    Update the form driver to for Turbo FormSubmission

--- app/javascript/drivers/form.js
+++ app/javascript/drivers/form.js
@@ -1,12 +1,29 @@
 import meta from '../meta'
 
-function invokeCommand(form, payload = {}) {
+function updateFormSubmission(formSubmission) {
+  const shouldUpdate =
+    formSubmission?.body instanceof URLSearchParams &&
+    formSubmission?.fetchRequest?.body instanceof URLSearchParams
+
+  if (!shouldUpdate) return
+
+  formSubmission.formData = new FormData(formSubmission.formElement)
+  for (const [key, value] of formSubmission.formData.entries()) {
+    formSubmission.fetchRequest.body.set(key, value)
+    formSubmission.body.set(key, value)
+  }
+
+  return formSubmission
+}
+
+function invokeCommand(form, payload = {}, event = {}) {
   payload.token = meta.token
   const input = document.createElement('input')
   input.type = 'hidden'
   input.name = 'turbo_boost_command'
   input.value = JSON.stringify(payload)
   form.appendChild(input)
+  updateFormSubmission(event.detail.formSubmission)
 }
 
 export default { invokeCommand }
