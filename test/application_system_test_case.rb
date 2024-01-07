56198ed04daec7e14a19e9145d4badba25a6ae50
Nate Hopkins (hopsoft) <natehop@gmail.com>
2024-01-07 11:16:34 -0700

    Attempt to make GH Actions happy

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -22,7 +22,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :null
 
   def self.playwright
-    @playwright ||= Playwright.create(playwright_cli_executable_path: Rails.root.join("../../node_modules/.bin/playwright"))
+    @playwright ||= Playwright.create(playwright_cli_executable_path: ENV.fetch("PLAYWRIGHT_CLI_EXECUTABLE_PATH", "/usr/local/bin/playwright"))
   end
 
   alias_method :orig_page, :page
