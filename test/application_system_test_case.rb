6653c8453ef088899a2d3a271f5f72412e7b2d92
Nate Hopkins (hopsoft) <natehop@gmail.com>
2024-01-07 18:15:10 -0700

    Give up

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -22,7 +22,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :null
 
   def self.playwright
-    @playwright ||= Playwright.create(playwright_cli_executable_path: ENV.fetch("PLAYWRIGHT_CLI_EXECUTABLE_PATH", "/usr/local/bin/playwright"))
+    @playwright ||= Playwright.create(playwright_cli_executable_path: ENV.fetch("PLAYWRIGHT_CLI_EXECUTABLE_PATH", "/app/node_modules/.bin/playwright"))
   end
 
   alias_method :orig_page, :page
