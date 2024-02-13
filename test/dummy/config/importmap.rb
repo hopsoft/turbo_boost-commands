0b36729e64f6741a06a503d8e890e1e48aebc9be
Nate Hopkins <natehop@gmail.com>
2024-02-13 14:26:25 -0700

    Add explicit version to JavaScript package to bust importmap CDN cache (#124)
    * Add explicit version to JavaScript package to bust importmap CDN cache
    
    * Sort importmap deps

--- test/dummy/config/importmap.rb
+++ test/dummy/config/importmap.rb
@@ -2,15 +2,14 @@
 
 # Pin npm packages by running ./bin/importmap
 
+pin "@alpinejs/morph", to: "@alpinejs--morph.js" # @3.13.5
 pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.2
 pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.2
 pin "@rails/actioncable", to: "@rails--actioncable.js" # @7.1.3
 pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @7.1.3
-
-# TODO: I have no idea why the fuck jspm.io refuses to build the latest versions of @turbo-boost libs
-#       You can generate URLs like the one I'm using below here → https://www.jsdelivr.com/github
-pin "@turbo-boost/streams", to: "https://cdn.jsdelivr.net/gh/hopsoft/turbo_boost-streams@v0.1.6/app/assets/builds/%40turbo-boost/streams.js"
+pin "@turbo-boost/streams", to: "@turbo-boost--streams.js" # @0.1.8
+pin "alpinejs" # @3.13.5
 
 # NOTE: The following libs stop working if we allow Rails to vendor them
 pin "flowbite", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.turbo.min.js"
@@ -22,5 +21,3 @@ FileUtils.ln_s Rails.root.join("../../app/assets/builds/@turbo-boost"), Rails.ro
 pin "@turbo-boost/commands", to: "@turbo-boost/commands.js"
 
 pin "application", preload: true
-pin "alpinejs" # @3.13.5
-pin "@alpinejs/morph", to: "@alpinejs--morph.js" # @3.13.5
