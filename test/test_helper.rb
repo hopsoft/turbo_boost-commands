4344d4ab9fb7af6875186109e8beb0f76c584ae3
Nate Hopkins (hopsoft) <natehop@gmail.com>
2024-02-23 11:40:05 -0700

    Seeing what standard config does

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,8 +8,8 @@ require "minitest/reporters"
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative "../test/dummy/config/environment"
-migrations_dir = File.expand_path("dummy/db/migrate", __dir__)
-ActiveRecord::MigrationContext.new(migrations_dir).migrate
-# ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/migrate", __dir__)]
-# ActiveRecord::Migrator.migrations_paths << File.expand_path("../db/migrate", __dir__)
-# require "rails/test_help"
+# migrations_dir = File.expand_path("dummy/db/migrate", __dir__)
+# ActiveRecord::MigrationContext.new(migrations_dir).migrate
+ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/migrate", __dir__)]
+ActiveRecord::Migrator.migrations_paths << File.expand_path("../db/migrate", __dir__)
+require "rails/test_help"
