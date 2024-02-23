f21a117aa147fb375a6a1a36a8d3c31ac47f2d12
Nate Hopkins (hopsoft) <natehop@gmail.com>
2024-02-22 23:44:01 -0700

    Switch to in-memory db

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,14 +12,6 @@ ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/mi
 ActiveRecord::Migrator.migrations_paths << File.expand_path("../db/migrate", __dir__)
 require "rails/test_help"
 
-# Load fixtures from the engine
-if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
-  ActiveSupport::TestCase.fixture_paths = [File.expand_path("fixtures", __dir__)]
-  ActionDispatch::IntegrationTest.fixture_paths = ActiveSupport::TestCase.fixture_paths
-  ActiveSupport::TestCase.file_fixture_path = File.expand_path("fixtures", __dir__) + "/files"
-  ActiveSupport::TestCase.fixtures :all
-end
-
 # ActionCable.server.config.logger = Logger.new($stdout) if ENV["VERBOSE"]
 #
 # module ActionViewTestCaseExtensions
