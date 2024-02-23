df117ff4a29b45a2597b060b79fa1d6ff4107a52
Nate Hopkins (hopsoft) <natehop@gmail.com>
2024-02-23 00:43:59 -0700

    Experiment

--- test/dummy/db/structure.sql
+++ test/dummy/db/structure.sql
@@ -1,7 +1,7 @@
-CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
-CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime(6) NOT NULL, "updated_at" datetime(6) NOT NULL);
 CREATE TABLE IF NOT EXISTS "users" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "session_id" varchar NOT NULL, "count" integer DEFAULT 0 NOT NULL, "created_at" datetime(6) NOT NULL, "updated_at" datetime(6) NOT NULL);
 CREATE TABLE sqlite_sequence(name,seq);
+CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
+CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime(6) NOT NULL, "updated_at" datetime(6) NOT NULL);
 INSERT INTO "schema_migrations" (version) VALUES
 ('20221230164522');
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,27 +8,6 @@ require "minitest/reporters"
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative "../test/dummy/config/environment"
-ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/migrate", __dir__)]
-ActiveRecord::Migrator.migrations_paths << File.expand_path("../db/migrate", __dir__)
-require "rails/test_help"
-
-# ActionCable.server.config.logger = Logger.new($stdout) if ENV["VERBOSE"]
-#
-# module ActionViewTestCaseExtensions
-#  def render(...)
-#    ApplicationController.renderer.render(...)
-#  end
-# end
-#
-# class ActiveSupport::TestCase
-#  include ActiveJob::TestHelper
-#  fixtures :all
-# end
-#
-# class ActionDispatch::IntegrationTest
-#  include ActionViewTestCaseExtensions
-# end
-#
-# class ActionCable::Channel::TestCase
-#  include ActionViewTestCaseExtensions
-# end
+# ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/migrate", __dir__)]
+# ActiveRecord::Migrator.migrations_paths << File.expand_path("../db/migrate", __dir__)
+# require "rails/test_help"
