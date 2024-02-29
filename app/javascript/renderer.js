e808867db3204ad4a66be61cdcf5f863d25adab8
Nate Hopkins (hopsoft) <natehop@gmail.com>
2024-02-29 01:28:40 -0700

    Cleanup

--- app/javascript/renderer.js
+++ app/javascript/renderer.js
@@ -1,24 +1,5 @@
 import uuids from './uuids'
 
-// Morphs the element with the given HTML via the TurboBoost invoke <turbo-stream>
-const morph = (selector, html) => {
-  const stream = document.createElement('turbo-stream')
-  stream.setAttribute('action', 'invoke')
-  stream.setAttribute('target', 'DOM')
-
-  const template = document.createElement('template')
-  template.content.textContent = JSON.stringify({
-    id: `morph-${uuids.v4()}`,
-    selector,
-    method: 'morph',
-    args: [html],
-    delay: 0
-  })
-
-  stream.appendChild(template)
-  document.body.appendChild(stream)
-}
-
 const append = content => {
   document.body.insertAdjacentHTML('beforeend', content)
 }
