0c1d5be2328fcf4286172cf9737e55972f8b852d
Nate Hopkins (hopsoft) <natehop@gmail.com>
2024-02-29 01:15:34 -0700

    Update to Streams 0.1.11

--- test/dummy/config/importmap.rb
+++ test/dummy/config/importmap.rb
@@ -6,8 +6,7 @@ pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.3
 pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.3
 pin "@rails/actioncable", to: "@rails--actioncable.js" # @7.1.3
-pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @7.1.3
-pin "@turbo-boost/streams", to: "@turbo-boost--streams.js" # @0.1.10
+pin "@turbo-boost/streams", to: "@turbo-boost--streams.js" # @0.1.11
 
 # NOTE: The following libs stop working if we allow Rails to vendor them
 pin "flowbite", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.turbo.min.js"
