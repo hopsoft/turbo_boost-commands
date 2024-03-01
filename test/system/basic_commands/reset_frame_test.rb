a81cfc448bc8f7f9c15586341f43983116e9ceb2
Nate Hopkins <natehop@gmail.com>
2024-02-29 22:10:48 -0700

    Comment skipped tests because appmapp sees them as failed (#131)

--- test/system/basic_commands/reset_frame_test.rb
+++ test/system/basic_commands/reset_frame_test.rb
@@ -9,51 +9,51 @@ class ResetFrameTest < ApplicationSystemTestCase
     dialog.accept
   end
 
-  test "increment once then reset and accept confirm" do
-    skip "Playwright dialog handling not working yet"
-    page.goto basic_command_url
-    user = User.last
+  # test "increment once then reset and accept confirm" do
+  #  skip "Playwright dialog handling not working yet"
+  #  page.goto basic_command_url
+  #  user = User.last
 
-    assert_equal 0, user.count
-    assert_equal "0000", page.wait_for_selector("code[role='counter']").inner_text
+  #  assert_equal 0, user.count
+  #  assert_equal "0000", page.wait_for_selector("code[role='counter']").inner_text
 
-    trigger = page.wait_for_selector("[data-turbo-command='IncrementCountCommand']")
-    trigger.click
-    wait_for_detach trigger
+  #  trigger = page.wait_for_selector("[data-turbo-command='IncrementCountCommand']")
+  #  trigger.click
+  #  wait_for_detach trigger
 
-    assert_equal 1, user.reload.count
-    assert_equal "0001", page.wait_for_selector("code[role='counter']").inner_text
+  #  assert_equal 1, user.reload.count
+  #  assert_equal "0001", page.wait_for_selector("code[role='counter']").inner_text
 
-    page.on :dialog, ->(dialog) { dialog.accept }
-    trigger = page.wait_for_selector("[data-turbo-command='ResetCountCommand']")
-    trigger.click
-    wait_for_detach trigger
+  #  page.on :dialog, ->(dialog) { dialog.accept }
+  #  trigger = page.wait_for_selector("[data-turbo-command='ResetCountCommand']")
+  #  trigger.click
+  #  wait_for_detach trigger
 
-    assert_equal 0, user.reload.count
-    assert_equal "0000", page.wait_for_selector("code[role='counter']").inner_text
-  end
+  #  assert_equal 0, user.reload.count
+  #  assert_equal "0000", page.wait_for_selector("code[role='counter']").inner_text
+  # end
 
-  test "increment once then reset and dismiss confirm" do
-    skip "Playwright dialog handling not working yet"
-    page.goto basic_command_url
-    user = User.last
+  # test "increment once then reset and dismiss confirm" do
+  #  skip "Playwright dialog handling not working yet"
+  #  page.goto basic_command_url
+  #  user = User.last
 
-    assert_equal 0, user.count
-    assert_equal "0000", page.wait_for_selector("code[role='counter']").inner_text
+  #  assert_equal 0, user.count
+  #  assert_equal "0000", page.wait_for_selector("code[role='counter']").inner_text
 
-    trigger = page.wait_for_selector("[data-turbo-command='IncrementCountCommand']")
-    trigger.click
-    wait_for_detach trigger
+  #  trigger = page.wait_for_selector("[data-turbo-command='IncrementCountCommand']")
+  #  trigger.click
+  #  wait_for_detach trigger
 
-    assert_equal 1, user.reload.count
-    assert_equal "0001", page.wait_for_selector("code[role='counter']").inner_text
+  #  assert_equal 1, user.reload.count
+  #  assert_equal "0001", page.wait_for_selector("code[role='counter']").inner_text
 
-    page.on :dialog, ->(dialog) { dialog.accept }
-    trigger = page.wait_for_selector("[data-turbo-command='ResetCountCommand']")
-    trigger.click
-    wait_for_detach trigger
+  #  page.on :dialog, ->(dialog) { dialog.accept }
+  #  trigger = page.wait_for_selector("[data-turbo-command='ResetCountCommand']")
+  #  trigger.click
+  #  wait_for_detach trigger
 
-    assert_equal 1, user.reload.count
-    assert_equal "0001", page.wait_for_selector("code[role='counter']").inner_text
-  end
+  #  assert_equal 1, user.reload.count
+  #  assert_equal "0001", page.wait_for_selector("code[role='counter']").inner_text
+  # end
 end
