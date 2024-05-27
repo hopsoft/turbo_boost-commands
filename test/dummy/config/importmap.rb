4c537d79e87268c84f4dd1cab7518583364f79fa
Nate Hopkins (hopsoft) <natehop@gmail.com>
2024-05-27 16:29:21 -0600

    Update deps

--- test/dummy/config/importmap.rb
+++ test/dummy/config/importmap.rb
@@ -3,7 +3,6 @@
 pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.4
 pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.4
-pin "@rails/actioncable", to: "@rails--actioncable.js" # @7.1.3
 pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @7.1.3
 pin "@turbo-boost/streams", to: "@turbo-boost--streams.js" # @0.1.11
 
